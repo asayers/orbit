943bb67e0403e54b6f56df6d858bf0ffca023208
Alex Sayers <alex.sayers@gmail.com>
2012-02-28 03:35:03 +0000

    clean-up

--- src/uk/co/oumu/orbit/Assets.java
+++ src/uk/co/oumu/orbit/Assets.java
@@ -2,9 +2,6 @@ package uk.co.oumu.orbit;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
